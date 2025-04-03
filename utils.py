82dd2e3f35d19886d2160763b50733f41de9229c
Rodrigo Barreto <rodrigobarretonunes19@gmail.com>
2025-04-03 16:36:32 +0200

    Implemented encryption function
    Developed the encrypt function to process text using a specific key. Implemented this via GitHub Mobile during a break at work.

--- utils.py
+++ utils.py
@@ -7,7 +7,23 @@ def key_gen():
     key=[random.randint(0,9) for _ in range(5)]
     return key
 
-
+def encrypt(text,key):
+    letters=list(string.ascii_letters)
+    encrypted_txt=""
+    for i,char in enumerate(text):
+        if char in letters:
+            shift=key[i%len(key)]
+            idx=letters.index(char)
+            new_idx=(idx+shift)%len(letters)
+            new_char=letters[new_idx]
+            encrypted_txt+=new_char
+        else:
+            encrypted_txt+=char
+    return encrypted_txt
+    
+    
+            
+            
 
    
 
