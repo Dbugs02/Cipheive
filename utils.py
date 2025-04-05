55febb257d6b6c57c98fa0684cc7fa755b3bf7df
Rodrigo Barreto <rodrigobarretonunes19@gmail.com>
2025-04-05 11:40:28 +0200

    Implemented decryption function

--- utils.py
+++ utils.py
@@ -19,9 +19,30 @@ def encrypt(text,key):
             encrypted_txt+=new_char
         else:
             encrypted_txt+=char
+    print(encrypted_txt)
     return encrypted_txt
-    
-    
+
+def decrypt(text,key):
+    letters=list(string.ascii_letters)
+    decrypted_txt=""
+    for i,char in enumerate(text):
+        if char in letters:
+            shift=key[i%len(key)]
+            idx=letters.index(char)
+            new_idx=(idx-shift)%len(letters)
+            new_char=letters[new_idx]
+            decrypted_txt+=new_char
+        else:
+            decrypted_txt+=char
+    print(decrypted_txt)
+    return decrypted_txt
+
+
+
+
+
+
+
             
             
 
